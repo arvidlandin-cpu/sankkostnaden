770c32b80edeab7fcb226ed251f9f7633288930b
arvidlandin-cpu <arvidlandin@gmail.com>
2026-09-19 17:33:17 +0200

    Use Next links in persistent mobile navigation

--- components/MobileQuickBar.tsx
+++ components/MobileQuickBar.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import { Wifi, Smartphone, ShieldCheck, Zap } from 'lucide-react';
 export default function MobileQuickBar(){
  return <nav className='mobileQuickBar' aria-label='Snabbnavigering'>
-  <a href='/bredband/'><Wifi size={16}/><b>Bredband</b></a>
-  <a href='/mobil/'><Smartphone size={16}/><b>Mobil</b></a>
-  <a href='/elavtal/'><Zap size={16}/><b>El</b></a>
-  <a href='/forsakring/'><ShieldCheck size={16}/><b>Försäkring</b></a>
+  <Link href='/bredband/'><Wifi size={16}/><b>Bredband</b></Link>
+  <Link href='/mobil/'><Smartphone size={16}/><b>Mobil</b></Link>
+  <Link href='/elavtal/'><Zap size={16}/><b>El</b></Link>
+  <Link href='/forsakring/'><ShieldCheck size={16}/><b>Försäkring</b></Link>
  </nav>
 }
