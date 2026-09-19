e023e0812f4d3ca28f36a43f38814ee956bbd314
arvidlandin-cpu <arvidlandin@gmail.com>
2026-09-19 12:07:28 +0200

    Set Sänk Kostnaden favicon explicitly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,13 +9,15 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel='manifest' href='/manifest.webmanifest' />
+        <link rel='icon' href='/app-icon.svg?v=2' type='image/svg+xml' />
+        <link rel='shortcut icon' href='/app-icon.svg?v=2' />
         <meta name='theme-color' content='#17201b' />
         <meta name='robots' content='index,follow,max-image-preview:large,max-snippet:-1,max-video-preview:-1' />
         <meta property='og:site_name' content='Sänk Kostnaden' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
         <meta name='apple-mobile-web-app-title' content='Sänk Kostnaden' />
-        <link rel='apple-touch-icon' href='/app-icon.svg' />
+        <link rel='apple-touch-icon' href='/app-icon.svg?v=2' />
       </Head>
       <Component {...pageProps} />
       <MobileQuickBar />
