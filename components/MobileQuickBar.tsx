8de6cba45aaa2ae39ee4423d02485e34e853f5c4
arvidlandin-cpu <arvidlandin@gmail.com>
2026-09-19 22:54:33 +0200

    Add finance to mobile navigation

--- components/MobileQuickBar.tsx
+++ components/MobileQuickBar.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
-import { Wifi, Smartphone, ShieldCheck, Zap } from 'lucide-react';
+import { Wifi, Smartphone, ShieldCheck, Zap, CircleDollarSign } from 'lucide-react';
 export default function MobileQuickBar(){
  return <nav className='mobileQuickBar' aria-label='Snabbnavigering'>
   <Link href='/bredband/'><Wifi size={16}/><b>Bredband</b></Link>
   <Link href='/mobil/'><Smartphone size={16}/><b>Mobil</b></Link>
   <Link href='/elavtal/'><Zap size={16}/><b>El</b></Link>
   <Link href='/forsakring/'><ShieldCheck size={16}/><b>Försäkring</b></Link>
+  <Link href='/ekonomi/'><CircleDollarSign size={16}/><b>Ekonomi</b></Link>
  </nav>
 }
