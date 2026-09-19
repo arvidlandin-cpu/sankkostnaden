9bebc455a4fc079e1de9c3797b4344302bc1b5c5
arvidlandin-cpu <arvidlandin@gmail.com>
2026-09-19 12:40:51 +0200

    Simplify mobile navigation to four core categories

--- components/MobileQuickBar.tsx
+++ components/MobileQuickBar.tsx
@@ -1,10 +1,9 @@
-import { PiggyBank, Wifi, Smartphone, ShieldCheck, Zap } from 'lucide-react';
+import { Wifi, Smartphone, ShieldCheck, Zap } from 'lucide-react';
 export default function MobileQuickBar(){
  return <nav className='mobileQuickBar' aria-label='Snabbnavigering'>
-  <a className='mqHome' href='/'><PiggyBank size={17}/><small>Start</small></a>
-  <a href='/bredband/'><Wifi size={15}/><b>Bredband</b></a>
-  <a href='/mobil/'><Smartphone size={15}/><b>Mobil</b></a>
-  <a href='/elavtal/'><Zap size={15}/><b>El</b></a>
-  <a href='/forsakring/'><ShieldCheck size={15}/><b>Försäkring</b></a>
+  <a href='/bredband/'><Wifi size={16}/><b>Bredband</b></a>
+  <a href='/mobil/'><Smartphone size={16}/><b>Mobil</b></a>
+  <a href='/elavtal/'><Zap size={16}/><b>El</b></a>
+  <a href='/forsakring/'><ShieldCheck size={16}/><b>Försäkring</b></a>
  </nav>
 }
