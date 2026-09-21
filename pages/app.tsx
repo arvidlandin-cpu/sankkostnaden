0f216c8d33322bccd6b1569a57a1c39656ed789e
arvidlandin-cpu <arvidlandin@gmail.com>
2026-09-21 09:37:21 +0200

    Add comparison CTA to Kostnadskollen header

--- pages/app.tsx
+++ pages/app.tsx
@@ -143,6 +143,7 @@ export default function SavingsApp() {
       <header className='topbar'>
         <Link className='brand' href='/'><span className='brandMark'><PiggyBank size={22} /></span><span>Sänk Kostnaden</span></Link>
         <nav><Link href='/bredband/'>Bredband</Link><Link href='/elavtal/'>El</Link><Link href='/mobil/'>Mobil</Link><Link href='/forsakring/'>Försäkring</Link><Link href='/ekonomi/'>Ekonomi</Link></nav>
+        <Link className='topbarCta' href='/#jamfor'>Jämför priser →</Link>
       </header>
 
       <main className={styles.appShell}>
